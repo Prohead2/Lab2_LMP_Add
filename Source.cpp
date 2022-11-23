a9284c75e7c2d9da12edc591d02b8a98884db55e
Prohead2 <114048927+Prohead2@users.noreply.github.com>
2022-11-23 16:21:59 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,58 @@
+#include "My_List.h"
+#include <Windows.h>
+
+void task(DLIST& list)
+{
+	ptrNODE p = list.get_begin();
+	int min_cost = p->info.GetCost();
+	while (p && p->info.GetCost() == min_cost)
+	{
+		
+		
+
+		p = p->next;
+	}
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	bool flag = true;
+	while (flag)
+	{
+		std::ifstream file("products.txt");
+		if (file)
+		{
+			DLIST list("products.txt");
+			file.close();
+			list.print();
+			task(list);
+		}
+
+		char choice;
+		bool label = true;
+		while (label)
+		{
+			std::cout << "Желаете начать заново или выйти? 1 - заново, 2 - выйти ";
+			std::cin >> choice;
+			switch (choice)
+			{
+			default:
+				std::cout << "Неверный ввод!" << std::endl;
+				label = true;
+				break;
+			case '1':
+				flag = true;
+				label = false;
+				break;
+			case '2':
+				flag = false;
+				label = false;
+				break;
+			}
+		}
+	}
+	std::cin.get();
+}
